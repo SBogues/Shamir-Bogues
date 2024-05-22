adf2f4380edb45f0e659e2c65e38b61dd1aba2b3
SBogues <sbogues@uvm.edu>
2024-05-22 18:10:39 -0500

    Lab 1: initial setup

---
+++ sitemap.php
@@ -0,0 +1,53 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>sb sitemap</title>
+    <meta name="author" content="Shamir Bogues">
+    <meta name="description" content="A collection of my remarkable assignments for Introduction to World Wide Web Design">
+    <style>
+        figure {
+            float: right;
+            text-align: center;
+            width: 25%;
+        }
+
+        img {
+            max-width: 100%;
+        }
+    </style>
+</head>
+
+<body>
+    <figure>
+        <img alt="Shamir Bogues Defense" src="images/shamir-bogues.jpeg">
+        <figcaption>Earning All Conference Awards.</figcaption>
+    </figure>
+
+    <h1>CS 1080-<em>OL</em> Summer 2024</h1>
+    <h2>Shamir Bogues - Site map</h2>
+    <p><a href="ADMIN/code-viewer.php">My Admin Folder</a></p>
+
+    <!--     ###########################################  -->
+    <section class="lab-layout">
+        <h2 class="header">Lab one - Setting up your sitemap.</h2>
+        <section class="public-files">
+            <h3>Public Files</h3>
+            <p><a href="sitemap.php">Site map</a></p>
+        </section>
+
+        <section class="supporting-files">
+            <h3>Supporting files</h3>
+            <p></p>
+        </section>
+
+        <section class="grader-notes">
+            <h3>Notes to grader</h3>
+            <p></p>
+        </section>
+    </section>
+    <!--     ###########################################  -->
+</body>
+
+</html>
