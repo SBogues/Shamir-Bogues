077c81eb55db9bd7e57f795e93d94283087d9cb2
SBogues <sbogues@uvm.edu>
2024-05-24 15:02:10 -0500

    Lab 2: Created Home page

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang+"en">
+<head>
+    <meta charset="utf-8">
+    <title>Restoring the Ocean</title>
+    <meta name="author" content="Shamir Bogues">
+    <meta name="description" content="Activities to combat pollution, mitigate the effects of climate change, and revive marine ecosystems are all part of the process of restoring the ocean. Restoration of coral reefs, reforestation of mangroves, sustainable fishing methods, and a decrease in plastic waste are important initiatives. The aims of these programs are to protect the health and resilience of ocean environments for future generations, increase biodiversity, and improve water quality.">
+</head>
+
+<body>
+    <main>
+        did this work
+    </main>
+</body>
+</html>
