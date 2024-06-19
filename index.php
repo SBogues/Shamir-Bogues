4b543b0802024bc30ef3fed9d284bca0a4adfb4d
SBogues <sbogues@uvm.edu>
2024-06-18 21:20:08 -0500

    Lab 2: Updated Home page

--- index.php
+++ index.php
@@ -1,15 +1,81 @@
 <!DOCTYPE HTML>
-<html lang+"en">
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <title>Restoring the Ocean</title>
+    <meta> 
+    <title>Making the world a better place</title>
     <meta name="author" content="Shamir Bogues">
-    <meta name="description" content="Activities to combat pollution, mitigate the effects of climate change, and revive marine ecosystems are all part of the process of restoring the ocean. Restoration of coral reefs, reforestation of mangroves, sustainable fishing methods, and a decrease in plastic waste are important initiatives. The aims of these programs are to protect the health and resilience of ocean environments for future generations, increase biodiversity, and improve water quality.">
+    <meta name="description" content="Making the world a better place refers to deeds and programs meant to uplift the standard of living for all living things and guarantee the planet's sustainability. This involves addressing issues like pollution, poverty, inequality, and climate change as well as social and economic issues. It includes advancing human rights, healthcare, education, and justice as well as supporting sustainable development, peace, and justice. For the benefit of the current and next generations, we may foster a more just, healthy, and successful society by promoting cooperation, creativity, and empathy."
 </head>
 
 <body>
     <main>
-        did this work
-    </main>
+        
+</main>
+<h1>Making the world a better place</h1>
+<section>
+    <h2>Creating a brighter future</h2>
+    <p>The first step in improving the world is to advance social fairness. This entails combating prejudice and making sure that everyone has access to fundamental rights like healthcare and education. By standing out for underrepresented groups, we build a more equitable society in which everyone may prosper</p>
+    <p>The growth of communities is essential to world peace. Change is sustained when grassroots programs concentrate on regional needs. Building stronger communities via improved access to economic, healthcare, and infrastructure encourages self-sufficiency and resilience, which makes the globe a more stable place.</p>
+    <p>Sustainability of the environment is essential to our future. Future generations can enjoy a healthy Earth thanks to pollution control and natural resource preservation. Reducing global warming and promoting sustainable behaviors both contribute to the upkeep of a robust and well-balanced environment.</p>
+    <p>International cooperation is essential to resolving global issues. Countries may pool resources and approaches to combat problems like illness and poverty by cooperating. A more inventive and globally conscious society is the result of international collaboration, which also makes the globe more peaceful and affluent.</p>
+    <section>
+
+    <section>
+        <h2>How to improve</h2>
+        <figure style="float: left; text-align: center; width: 20%;">
+            <img alt="Recycling" src="images/Recycling.jpeg" style="max-width: 100%;">
+            <figcation><cite><a href="https://www.freepik.com/free-vector/people-recycling-together_7884850.htm" target="_blank">Recycling Together</a></cite></figurecaption>
+</figure>
+<ol>
+    <li>Reduce, Reuse, and Recycle</li>
+    <li>Conserve Energy</li>
+    <li>Volunteer</li>
+    <li>Conserve Water</li>
+    <li>Plant Trees</li>
+    <li>Protect Wildlife</li>
+    <li>Source: <cite>Shamir Bogues</cite></li>
+</ol>
+</section>
+<section>
+    <h2>Actions to mae the World a Better Place</h2>
+    <table>
+        <caption>Thing we need to do better</caption>
+        
+        <tr>
+            <th>Action</th>
+            <th>Impact</th>
+            <th>Resources</th>
+</tr>
+
+<tr>
+    <td>Educate others</td>
+    <td>Raises awareness and fosters informed decisions</td>
+    <td>TED Talks</td>
+</tr>
+
+<tr>
+    <td>Donate</td>
+    <td>Provides financial support to important causes</td>
+    <td>World Vision</td>
+</tr>
+
+<tr>
+    <td>Advocate for Policy Change</td>
+    <td>Influences systemic change</td>
+    <td>Change.org</td>
+</tr>
+
+<tr>
+    <td>Eat Sustainably</td>
+    <td>Reduces environmental impact of food production</td>
+    <td>Sustainable Food Resources</td>
+</tr>
+<tr>
+    <td colspan="3">Source: <cite>Shamir Bogues</cite>
+    <br><cite><a href="https://reflectionsfromaredhead.com/make-the-world-a-better-place/" target="_blank">Things we need to do better</a></cite></td>
+</tr>
+</table>
+</section>
+</main>
 </body>
 </html>
