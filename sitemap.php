b3772cc1e3d184d0a6704dabf13e6bfd7acbc787
SBogues <sbogues@uvm.edu>
2024-06-20 12:58:49 -0500

    Lab 3-B: updated site map

--- sitemap.php
+++ sitemap.php
@@ -3,9 +3,9 @@
 
 <head>
     <meta charset="utf-8">
-    <title>sb sitemap</title>
+    <title>my sitemap</title>
     <meta name="author" content="Shamir Bogues">
-    <meta name="description" content="A collection of my remarkable assignments for Introduction to World Wide Web Design">
+    <meta name="description" content="A portfolio of my wonderful work for my Introduction to World Wide Web Design">
     <style>
         figure {
             float: right;
@@ -21,13 +21,59 @@
 
 <body>
     <figure>
-        <img alt="Shamir Bogues Defense" src="images/shamir-bogues.jpeg">
-        <figcaption>Earning All Conference Awards.</figcaption>
+        <img alt="Shamir Bogues all conference award" src="images/shamir-bogues.jpeg">
+        <figcaption>Winning an All Conference Awward.</figcaption>
     </figure>
 
-    <h1>CS 1080-<em>OL</em> Summer 2024</h1>
+    <h1>CS 1080-<em>ol</em> Summer 2024</h1>
     <h2>Shamir Bogues - Site map</h2>
     <p><a href="ADMIN/code-viewer.php">My Admin Folder</a></p>
+
+    <!--     ###########################################  -->
+ <section class="lab-layout">
+    <h2 class="header">Lab Three B - Creating a website.</h2>
+    <section class="public-files">
+        <h3>Public Files</h3>
+        <p><a href="lab3/about.php">About page</a></p>
+        <p><a href="lab3/detail.php">Detail page</a></p>
+        <p><a href="lab3/form.php">Form page</a></p>
+        <p><a href="lab3/index.php">Home page</a></p>
+    </section>
+
+    <section class="supporting-files">
+        <h3>Supporting files</h3>
+        <p><a href="lab3/css/custom.css">css style sheet</a></p>
+        <p><a href="lab3/images/wireframe.png">wireframe</a></p>
+    </section>
+
+    <section class="grader-notes">
+        <h3>Notes to grader</h3>
+        <p></p>
+    </section>
+</section>
+ 
+    <!--     ###########################################  -->
+ <section class="lab-layout">
+    <h2 class="header">Lab Three - Creating a website.</h2>
+    <section class="public-files">
+        <h3>Public Files</h3>
+        <p><a href="lab3/about.php">About page</a></p>
+        <p><a href="lab3/detail.php">Detail page</a></p>
+        <p><a href="lab3/index.php">Home page</a></p>
+    </section>
+
+    <section class="supporting-files">
+        <h3>Supporting files</h3>
+        <p><a href="lab3/css/custom.css">css style sheet</a></p>
+        <p><a href="lab3/images/wireframe.png">wireframe</a></p>
+    </section>
+
+    <section class="grader-notes">
+        <h3>Notes to grader</h3>
+        <p></p>
+    </section>
+</section>
+
 
     <!--     ###########################################  -->
     <section class="lab-layout">
@@ -44,13 +90,13 @@
 
         <section class="grader-notes">
             <h3>Notes to grader</h3>
-            <p>Lab topic is Restoring the Ocean</p>
-            <p>Final project topic is about Australian Wildlife</p>
+            <p>Lab topic is Making the world a better place</p>
+            <p>Final Project topic is Australian Wildlife</p>
         </section>
     </section>
 
-     <!--     ###########################################  -->
-     <section class="lab-layout">
+    <!--     ###########################################  -->
+    <section class="lab-layout">
         <h2 class="header">Lab one - Setting up your sitemap.</h2>
         <section class="public-files">
             <h3>Public Files</h3>
